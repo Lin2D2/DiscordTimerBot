201b26a013f25b53c5055a06df064dace4e13240
Linus Behrens <linus@thebehrens.net>
2021-10-22 15:34:10 +0200

    clean up

--- package/bot.py
+++ package/bot.py
@@ -32,13 +32,12 @@ class Bot(discord.Client):
 
         if user_id in self.user_id_map.keys():
             if content.find("gleich") != -1:
-                await self.timer_gleich(user_id, channel)
+                await self.timer(channel, user_id, "gleich", 600)
 
         else:
             print(f"{user_name}, id: {user_id}")
 
-    async def timer_gleich(self, user_id, channel):
-        seconds = 300
+    async def timer(self, channel, user_id, timer_type, seconds, edit=False):
         current_time_step = time.time()
         if seconds > 60 * 5:
             time_string = f"```diff\n+\"[{time.strftime('%H:%M:%S', time.gmtime(seconds))}]\"```"
@@ -48,17 +47,25 @@ class Bot(discord.Client):
             time_string = f"```fix\n+\"[{time.strftime('%H:%M:%S', time.gmtime(seconds))}]\"```"
         else:
             time_string = f"```css\n+\"[{time.strftime('%H:%M:%S', time.gmtime(seconds*-1))}]\"```"
-        message = await channel.send(
-            embed=self.normal_message_embed(
-                f"{self.user_id_map[user_id]}: gleich",
-                time_string
+        if not edit:
+            message = await channel.send(
+                embed=self.normal_message_embed(
+                    f"{self.user_id_map[user_id]}: {timer_type}",
+                    time_string
+                )
             )
-        )
-        self.running_timers.append((message, user_id, "gleich", seconds, current_time_step))  # message, id, type, sec
-        if not self.timer_loop_task:
-            self.timer_loop_task = asyncio.create_task(self.timer_loop())
-            await self.timer_loop_task
-            self.timer_loop_task = None
+            self.running_timers.append((message, user_id, timer_type, seconds, current_time_step))
+            if not self.timer_loop_task:
+                self.timer_loop_task = asyncio.create_task(self.timer_loop())
+                await self.timer_loop_task
+                self.timer_loop_task = None
+        else:
+            await channel.edit(
+                embed=self.normal_message_embed(
+                    f"{self.user_id_map[user_id]}: {timer_type}",
+                    time_string
+                )
+            )
 
     @staticmethod
     def normal_message_embed(title, message, color=0xdb2c2c):
@@ -80,22 +87,7 @@ class Bot(discord.Client):
                 time_diff = current_time_step - last_time_step
                 seconds -= time_diff
                 self.running_timers[index] = (message, user_id, timer_type, seconds, current_time_step)
-                # TODO dosen`t work
-                if seconds > 60 * 5:
-                    time_string = f"```diff\n+\"[{time.strftime('%H:%M:%S', time.gmtime(seconds))}]\"```"
-                elif seconds > 60:
-                    time_string = f"```yaml\n+\"[{time.strftime('%H:%M:%S', time.gmtime(seconds))}]\"```"
-                elif seconds > 0:
-                    time_string = f"```fix\n+\"[{time.strftime('%H:%M:%S', time.gmtime(seconds))}]\"```"
-                else:
-                    time_string = f"```css\n+\"[-{time.strftime('%H:%M:%S', time.gmtime(seconds*-1))}]\"```"
-                await message.edit(
-                    embed=self.normal_message_embed(
-                        # TODO change color for time and handle negative time
-                        f"{self.user_id_map[user_id]}: {timer_type}",
-                        time_string
-                    )
-                )
+                await self.timer(message, user_id, timer_type, seconds, edit=True)
             time_taken = time.time() - start_time
             print(f"time taken: {time_taken}")
             if time_taken < 1:
