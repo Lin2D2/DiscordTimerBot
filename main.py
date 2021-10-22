dfbf68aa3a1cd7c36b115e7831e5b310528a8d61
Linus Behrens <linus@thebehrens.net>
2021-10-22 15:02:54 +0200

    inital commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from package.bot import Bot
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+if __name__ == "__main__":
+    bot = Bot()
+    # bot.run(os.getenv('DISCORD_BOT_TOKEN'))
+    bot.run("OTAwODQxNDE5OTQwNDYyNjMz.YXHL8w.hkAogCp5uaANohZikTH4ylgKL3E")
