7fcccad78f297078b16e56225a0c9cdf099c9e09
Linus Behrens <linus@thebehrens.net>
2021-10-22 15:06:00 +0200

    quick fix

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from package.bot import Bot
-# import os
-# from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 if __name__ == "__main__":
     bot = Bot()
-    # bot.run(os.getenv('DISCORD_BOT_TOKEN'))
-    bot.run("OTAwODQxNDE5OTQwNDYyNjMz.YXHL8w.hkAogCp5uaANohZikTH4ylgKL3E")
+    bot.run(os.getenv('DISCORD_BOT_TOKEN'))
